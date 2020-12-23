7fef4933bb750cd35de86dfcbfb8f84d35e6a15d
Alan <alan.bontoft@gmail.com>
2020-12-23 08:55:31 +0000

    Comments added to main.cpp

--- main.cpp
+++ main.cpp
@@ -18,14 +18,16 @@ int main(int argc, char *argv[])
 
     qmlRegisterUncreatableType<DataModel>("com.druck.models", 1, 0, "DataModel", "DataModel is uncreatable type");
 
+    // declare data model objects
     DataModel model1, model2;
 
+    // initialise models
     model1.setSalary(10000);
-
     model2.setSalary(20000);
 
     QQmlApplicationEngine engine;
 
+    // make data models visible to QML
     engine.rootContext()->setContextProperty("TheModelOne", &model1);
     engine.rootContext()->setContextProperty("TheModelTwo", &model2);
 
